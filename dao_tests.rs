daf9eaa66a05c1ac6f01d19d8837ec595d1a7965
Staking Agency <contact@staking.agency>
2025-04-01 23:06:49 +0300

    improved test

--- dao_tests.rs
+++ dao_tests.rs
@@ -189,8 +189,8 @@ fn dao_execute_new_launchpad_test() {
     let max_buy_amount = exp18(100);
     let title = "Launchpad proposal".as_bytes().to_vec();
     let description = "Launchpad proposal description".as_bytes().to_vec();
-    let start_time = DAO_VOTING_PERIOD + 1;
-    let end_time = start_time + 10;
+    let mut start_time = DAO_VOTING_PERIOD + 1;
+    let mut end_time = start_time + 10;
     let mut proposal_id = 0;
     // propose new launchpad
     sc_setup.blockchain_wrapper
@@ -202,9 +202,9 @@ fn dao_execute_new_launchpad_test() {
                     kyc_enforced: true,
                     token: managed_token_id!(FRANCHISE1_GOVERNANCE_TOKEN_ID),
                     payment_token: managed_token_id!(PAYMENT_TOKEN_ID),
-                    price: price.into(),
-                    min_buy_amount: min_buy_amount.into(),
-                    max_buy_amount: max_buy_amount.into(),
+                    price: price.clone().into(),
+                    min_buy_amount: min_buy_amount.clone().into(),
+                    max_buy_amount: max_buy_amount.clone().into(),
                     start_time,
                     end_time,
                 },
@@ -225,6 +225,8 @@ fn dao_execute_new_launchpad_test() {
         .assert_ok();
     // advance the time to end the voting period
     sc_setup.blockchain_wrapper.set_block_timestamp(DAO_VOTING_PERIOD);
+    start_time += DAO_VOTING_PERIOD;
+    end_time += DAO_VOTING_PERIOD;
     // execute the proposal
     sc_setup.blockchain_wrapper
         .execute_tx(&sc_setup.owner, &sc_setup.dao_wrapper, &big_zero, |sc| {
@@ -237,4 +239,23 @@ fn dao_execute_new_launchpad_test() {
             assert!(!sc.last_launchpad_id().is_empty());
         })
         .assert_ok();
+    // propose same launchpad should fail as it is already on launchpad
+    sc_setup.blockchain_wrapper
+        .execute_tx(&sc_setup.owner, &sc_setup.dao_wrapper, &big_zero, |sc| {
+            sc.propose_new_launchpad(
+                managed_buffer!(&title),
+                managed_buffer!(&description),
+                LaunchpadProposal{
+                    kyc_enforced: true,
+                    token: managed_token_id!(FRANCHISE1_GOVERNANCE_TOKEN_ID),
+                    payment_token: managed_token_id!(PAYMENT_TOKEN_ID),
+                    price: price.into(),
+                    min_buy_amount: min_buy_amount.into(),
+                    max_buy_amount: max_buy_amount.into(),
+                    start_time,
+                    end_time,
+                },
+            );
+        })
+        .assert_user_error(err2str(ERROR_TOKEN_ALREADY_LAUNCHED));
 }
